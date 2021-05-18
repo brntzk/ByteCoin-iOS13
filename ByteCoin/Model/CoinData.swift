2446c7a4fd7104083b58139b56d1968d6c4ee2c0
Birant <birantozek@gmail.com>
2021-05-18 07:47:49 +0300

    Proje Tamamlandı
    API ile veriler çekildi farklı birimlere göre değer çekilmesi için proje tamamlandı

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,18 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Birant Özek on 24.03.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+
+}
+
+
+
+//main.temp
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,13 +7,70 @@
 //
 
 import Foundation
+import UIKit
+
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 
 struct CoinManager {
     
+    var delegate: CoinManagerDelegate?
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","TRY"]
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "api key here"
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    
+    
+    func getCoinPrice(forCurrency: String) {
+        let urlString = "\(baseURL)/\(forCurrency)?apikey=\(apiKey)"
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let bitcoinPrice = parseJSON(safeData){
+                        let priceString = String(format: "%.2f", bitcoinPrice)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: forCurrency)
+                    }
+                }
+//                let dataAsString = String(data: data!, encoding: .utf8)
+//                print(dataAsString)
+                
+            }
+            
+            task.resume()
+        }
+        
+        func parseJSON (_ data: Data) -> Double? {
+            let decoder = JSONDecoder()
+            do {
+                let decodedData = try decoder.decode(CoinData.self, from: data)
+                let lastPrice = decodedData.rate
+                print(lastPrice)
+                return lastPrice
+                
+            } catch {
+                print(error)
+                return nil
+            }
+        }
+    }
 }
+//        https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=A4E60405-C091-43BD-8777-5C651CF92D13
+//        rate
+//weather[0].description
+//weather[0].main
+//weather[0].id
+//main.temp
+//name
+
+
+//rate
